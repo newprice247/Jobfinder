604e7772a6729d9782877b1890b6f6e51b5b83eb
newprice247 <newprice247@gmail.com>
2023-11-26 15:09:52 -0500

    m

--- server/server.js
+++ server/server.js
@@ -1,35 +1,25 @@
-require('dotenv').config({
-  path: `.env.${process.env.NODE_ENV}`,
-});
+require('dotenv').config({ path: './.env' });
 
 const express = require('express');
 const path = require('path');
 
 const db = require('./config/connection');
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT
 const app = express();
 
-const startServer = async () => {
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
-  app.use(express.urlencoded({ extended: false }));
-  app.use(express.json());
+app.use(express.static(path.join(__dirname, '../client/dist')));
 
-  if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/dist')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+});
 
-    app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-    });
-  }
-
-  db.once('open', () => {
-    app.listen(PORT, () => {
-      console.log(`API server running on port ${PORT}!`);
-      
-    });
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
   });
-};
-
-startServer();
+});
 
