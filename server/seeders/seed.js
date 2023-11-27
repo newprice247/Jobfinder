00ae478c2dcbb77b713ce91b2e096b6a21df744f
newprice247 <newprice247@gmail.com>
2023-11-26 22:11:04 -0500

    add seed data

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -5,25 +5,25 @@ const Card = require('../models/Card');
 const cards = [
     {
         title: 'Card 1',
-        subtitle: 'Subtitle 1',
-        text: 'Text 1',
-        link: 'Link 1',
-        linkName: 'Link Name 1'
+        subtitle: "I'm being pulled from the cloud!",
+        text: 'This is all being mapped over with a .map() function!',
+        link: 'https://project3mernstack-c25ab63e2028.herokuapp.com/',
+        linkName: 'Heroku Deployment'
     },
     {
         title: 'Card 2',
-        subtitle: 'Subtitle 2',
-        text: 'Text 2',
-        link: 'Link 2',
-        linkName: 'Link Name 2'
+        subtitle: "I'm being pulled from the cloud too!",
+        text: 'This is all being mapped over with a .map() function!',
+        link: 'https://github.com/newprice247/Project3',
+        linkName: 'GitHub Repository'
     },
     {
         title: 'Card 3',
-        subtitle: 'Subtitle 3',
-        text: 'Text 3',
-        link: 'Link 3',
-        linkName: 'Link Name 3'
-    },
+        subtitle: "I'm being pulled from the cloud as well!",
+        text: 'This is all being mapped over with a .map() function!',
+        link: 'https://www.google.com/',
+        linkName: 'Google'
+    }
 ];
 
 // Connects to the database, deletes the users and thoughts collections if they exist already, creates the users and thoughts collections, and loops through each thought and pushes the thought's _id to the appropriate user's thoughts array field
