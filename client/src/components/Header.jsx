55fa549d9d79fe94c55d3943346c4f3fa58785d8
yesenia <yeseniab743@gmail.com>
2023-12-05 12:31:53 -0500

    change color

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -59,7 +59,7 @@ export default function StickyNavbar() {
 
   return (
     <div className="-m-6 max-h-[768px] w-[calc(100%+48px)] overflow-scroll">
-      <Navbar className="sticky top-0 z-10 h-max max-w-full rounded-none px-4 py-2 lg:px-8 lg:py-4 bg-myColor-5">
+      <Navbar className="sticky top-0 z-10 h-max max-w-full rounded-none px-4 py-2 lg:px-8 lg:py-4 bg-myColor-1">
         <div className="flex items-center justify-between text-blue-gray-900">
           <Link
             to={'/'}
