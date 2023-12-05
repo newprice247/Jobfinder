aab4d0f79ad4ccf29aff0f01ff4176a9a82d0f87
yesenia <yeseniab743@gmail.com>
2023-12-05 12:07:02 -0500

    added img and change color

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -59,7 +59,7 @@ export default function StickyNavbar() {
 
   return (
     <div className="-m-6 max-h-[768px] w-[calc(100%+48px)] overflow-scroll">
-      <Navbar className="sticky top-0 z-10 h-max max-w-full rounded-none px-4 py-2 lg:px-8 lg:py-4 bg-myColor-2">
+      <Navbar className="sticky top-0 z-10 h-max max-w-full rounded-none px-4 py-2 lg:px-8 lg:py-4 bg-myColor-5">
         <div className="flex items-center justify-between text-blue-gray-900">
           <Link
             to={'/'}
@@ -70,7 +70,7 @@ export default function StickyNavbar() {
             href="#"
             className="ml-4 cursor-pointer py-1.5 font-medium"
           >
-            Jobfinder
+          <img src="client/src/assets/images/logo.jpg" alt="jobfinder" />
           </Typography>
           </Link>
           <div className="flex items-center gap-4">
