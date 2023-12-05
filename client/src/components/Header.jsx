81c1eb02a7449636a6789eb04cbc74a8027f582f
newprice247 <newprice247@gmail.com>
2023-12-05 13:35:34 -0500

    add dynamic buttons to header based on user logged in

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -21,55 +21,76 @@ export default function StickyNavbar() {
     );
   }, []);
 
-  const navList = (
-    <ul className="mt-2 mb-4 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
-      <Link
-        to={'/'}
-        className="text-lg"
-      >
-        <Typography
-          as="li"
-          variant="small"
-          color="blue-gray"
-          className="p-1 font-normal"
-        >
-          <p className="flex items-center">
-            Home
-          </p>
-        </Typography>
-      </Link>
-
-    </ul>
-  );
-
   return (
-    <div className="-m-6 max-h-[768px] w-[calc(100%+48px)] overflow-scroll">
+    <div>
       <Navbar className="sticky top-0 z-10 h-max max-w-full rounded-none px-4 py-2 lg:px-8 lg:py-4 bg-myColor-2">
         <div className="flex items-center justify-between text-blue-gray-900">
           <Link
             to={'/'}
             className="text-lg"
           >
-          <Typography
-            as="a"
-            href="#"
-            className="ml-4 cursor-pointer py-1.5 font-medium"
-          >
-            Jobfinder
-          </Typography>
-          </Link>
-          <div className="flex items-center gap-4">
-            <div className="mr-4 hidden lg:block">{navList}</div>
-            <div className="flex items-center gap-x-1">
-            <Link
-            to={'/login'}
-            className=""
+            <Typography
+              as="a"
+              href="#"
+              className="ml-4 cursor-pointer py-1.5 font-medium"
             >
-              <Button fullWidth variant="text" size="sm" className="">
-                <span>
-                  {Auth.loggedIn() ? (
-                    <>
-                      <Link to="/profile">
+              Jobfinder
+            </Typography>
+          </Link>
+          <div className="flex-row gap-4 justify-center">
+            <div className="mr-4 hidden lg:block">
+              {Auth.loggedIn() ? (
+                <>
+                  <Link to="/user-profile">
+                    <Button
+                      color="blue-gray"
+                      buttonType="link"
+                      size="sm"
+                      rounded={false}
+                      block={false}
+                      iconOnly={false}
+                      ripple="light"
+                    >
+                      My Profile
+                    </Button>
+                  </Link>
+                  <Link to="/">
+                    <Button
+                      color="blue-gray"
+                      buttonType="link"
+                      size="sm"
+                      rounded={false}
+                      block={false}
+                      iconOnly={false}
+                      ripple="light"
+                    >
+                      Home
+                    </Button>
+                  </Link>
+                </>
+              ) : (
+                <Link to="/">
+                  <Button
+                    color="blue-gray"
+                    buttonType="link"
+                    size="sm"
+                    rounded={false}
+                    block={false}
+                    iconOnly={false}
+                    ripple="light"
+                  >
+                    Home
+                  </Button>
+                </Link>
+              )
+              }
+              <Link
+                to={'/login'}
+                className=""
+              >
+                <Button fullWidth variant="text" size="sm" className="">
+                    {Auth.loggedIn() ? (
+                      <>
                         <Button
                           color="blue-gray"
                           buttonType="link"
@@ -78,58 +99,44 @@ export default function StickyNavbar() {
                           block={false}
                           iconOnly={false}
                           ripple="light"
+                          onClick={Auth.logout}
                         >
-                          Profile
+                          Logout
                         </Button>
-                      </Link>
-                      <Button
-                        color="blue-gray"
-                        buttonType="link"
-                        size="sm"
-                        rounded={false}
-                        block={false}
-                        iconOnly={false}
-                        ripple="light"
-                        onClick={Auth.logout}
-                      >
-                        Logout
-                      </Button>
-                    </>
-                  ) : (
-                    <>
-                      <Link to="/register">
-                        <Button
-                          color="blue-gray"
-                          buttonType="link"
-                          size="sm"
-                          rounded={false}
-                          block={false}
-                          iconOnly={false}
-                          ripple="light"
-                        >
-                          Sign Up
-                        </Button>
-                      </Link>
-                      <Link to="/login">
-                        <Button
-                          color="blue-gray"
-                          buttonType="link"
-                          size="sm"
-                          rounded={false}
-                          block={false}
-                          iconOnly={false}
-                          ripple="light"
-                        >
-                          Login
-                        </Button>
-                      </Link>
-                    </>
-                  )}
+                      </>
+                    ) : (
+                      <>
+                        <Link to="/register">
+                          <Button
+                            color="blue-gray"
+                            buttonType="link"
+                            size="sm"
+                            rounded={false}
+                            block={false}
+                            iconOnly={false}
+                            ripple="light"
+                          >
+                            Sign Up
+                          </Button>
+                        </Link>
+                        <Link to="/login">
+                          <Button
+                            color="blue-gray"
+                            buttonType="link"
+                            size="sm"
+                            rounded={false}
+                            block={false}
+                            iconOnly={false}
+                            ripple="light"
+                          >
+                            Login
+                          </Button>
+                        </Link>
+                      </>
+                    )}
+                </Button>
+              </Link>
 
-                </span>
-              </Button>
-            </Link>
-              
             </div>
             <IconButton
               variant="text"
@@ -170,19 +177,99 @@ export default function StickyNavbar() {
             </IconButton>
           </div>
         </div>
-        {/* <MobileNav open={openNav}>
-          {navList}
-          <div className="flex items-center gap-x-1">
-            <Link
-            to={'/login'}
-            className=""
-            >
-              <Button fullWidth variant="text" size="sm" className="">
-                <span>Log In</span>
-              </Button>
-            </Link>
+        <MobileNav open={openNav}>
+          <div className="grid-cols-1 items-center gap-x-1">
+            {
+              Auth.loggedIn() ? (
+                <>
+                <Link to="/">
+                    <Button
+                      color="blue-gray"
+                      buttonType="link"
+                      size="sm"
+                      rounded={false}
+                      block={false}
+                      iconOnly={false}
+                      ripple="light"
+                    >
+                      Home
+                    </Button>
+                  </Link>
+                  <Link to="/user-profile">
+                    <Button
+                      color="blue-gray"
+                      buttonType="link"
+                      size="sm"
+                      rounded={false}
+                      block={false}
+                      iconOnly={false}
+                      ripple="light"
+                    >
+                      My Profile
+                    </Button>
+                  </Link>
+                  <Button
+                    color="blue-gray"
+                    buttonType="link"
+                    size="sm"
+                    rounded={false}
+                    block={false}
+                    iconOnly={false}
+                    ripple="light"
+                    onClick={Auth.logout}
+                  >
+                    Logout
+                  </Button>
+                  
+                </>
+              ) : (
+                <div>
+
+               
+                <Link to="/">
+                  <Button
+                    color="blue-gray"
+                    buttonType="link"
+                    size="sm"
+                    rounded={false}
+                    block={false}
+                    iconOnly={false}
+                    ripple="light"
+                  >
+                    Home
+                  </Button>
+                </Link>
+                <Link to="/register">
+                  <Button
+                    color="blue-gray"
+                    buttonType="link"
+                    size="sm"
+                    rounded={false}
+                    block={false}
+                    iconOnly={false}
+                    ripple="light"
+                  >
+                    Sign Up
+                  </Button>
+                </Link>
+                <Link to="/login">
+                  <Button
+                    color="blue-gray"
+                    buttonType="link"
+                    size="sm"
+                    rounded={false}
+                    block={false}
+                    iconOnly={false}
+                    ripple="light"
+                  >
+                    Login
+                  </Button>
+                </Link>
+                </div>
+              )
+            }
           </div>
-        </MobileNav> */}
+        </MobileNav>
       </Navbar>
     </div>
   );
