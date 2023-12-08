d894166a128b1f04f5acda74b0568ea469e36c44
kwaters3 <knickler3@gmail.com>
2023-12-08 16:39:59 -0500

    adding toggle buttons

--- client/src/components/Searchbar.jsx
+++ client/src/components/Searchbar.jsx
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useState } from 'react';
 import { Button } from "@material-tailwind/react";
-import { motion } from "framer-motion"
+import { motion } from "framer-motion";
+import {
+  Collapse,
+  Card,
+  Typography,
+  CardBody,
+} from "@material-tailwind/react";
  
-export default function ButtonDefault() {
+export default function searchBar() {
+
+  const [open, setOpen] = React.useState(false);
+ 
+  const toggleOpen = () => setOpen((cur) => !cur);
+
   return (
     <div>
           {/* Using 'motion' to animate the homepage, set as a div container with opening and closing motion.div tags */}
@@ -12,11 +23,33 @@ export default function ButtonDefault() {
               viewport={{ once: true }}
               animate={{ y: 70}}
               transition={{ delay: 0.5, duration: 0.5 }}
-              className="flex flex-wrap justify-center items-center">
+              className="flex flex-wrap gap-3 justify-center items-center">
   
   <Button >Search Here</Button>
+
+  <Button onClick={toggleOpen}>Job Category</Button>
+      <Collapse open={open}>
+        <Card className="my-4 mx-auto w-8/12">
+          <CardBody>
+            <Typography>
+            job categories listings: food/beverage, retail, etc.
+            </Typography>
+          </CardBody>
+        </Card>
+      </Collapse>
+
+      
+  <Button onClick={toggleOpen}>Job Category</Button>
+      <Collapse open={open}>
+        <Card className="my-4 mx-auto w-8/12">
+          <CardBody>
+            <Typography>
+            job categories listings: food/beverage, retail, etc.
+            </Typography>
+          </CardBody>
+        </Card>
+      </Collapse>
   
-  <h1>blank text</h1>
     </motion.div>
     </div>
   );
