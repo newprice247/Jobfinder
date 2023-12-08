796cc582540828526896ab08e1463bf59f068028
newprice247 <newprice247@gmail.com>
2023-12-07 20:04:57 -0500

    add tabs to user profile

--- client/src/pages/UserProfile.jsx
+++ client/src/pages/UserProfile.jsx
@@ -1,141 +1,80 @@
-import React, { useState, useEffect } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { PaperClipIcon } from '@heroicons/react/20/solid';
+import React, { useEffect, useState } from "react";
+import {
+  Tabs,
+  TabsHeader,
+  TabsBody,
+  Tab,
+  TabPanel,
+} from "@material-tailwind/react";
+import { FontAwesomeIcon } from "../../node_modules/@fortawesome/react-fontawesome";
 import {
   faYoutube,
   faFacebook,
   faTwitter,
   faInstagram
 } from "@fortawesome/free-brands-svg-icons";
+import {
+  Square3Stack3DIcon,
+  UserCircleIcon,
+  Cog6ToothIcon,
+} from "@heroicons/react/24/solid";
+
+import Bio from "../components/Bio";
 
 export default function UserProfile() {
+  
   return (
-    <div>
-    <a href="https://www.youtube.com/c/jamesqquick"
-        className="youtube social">
-        <FontAwesomeIcon icon={faYoutube} size="2x" />
-      </a>
-      <a href="https://www.facebook.com/learnbuildteach/"
-        className="facebook social">
-        <FontAwesomeIcon icon={faFacebook} size="2x" />
-      </a>
-      <a href="https://www.twitter.com/jamesqquick" className="twitter social">
-        <FontAwesomeIcon icon={faTwitter} size="2x" />
-      </a>
-      <a href="https://www.instagram.com/learnbuildteach"
-        className="instagram social">
-        <FontAwesomeIcon icon={faInstagram} size="2x" />
-      </a>
-      
-    <div>
-      <div className="px-4 sm:px-0">
-        <h3 className="text-base font-semibold leading-7 text-gray-900">Users profile</h3>
-        <p className="mt-1 max-w-2xl text-sm leading-6 text-gray-500">Personal details and resumes.</p>
+    <>
+      <div>
+        <a href="https://www.youtube.com/c/jamesqquick"
+          className="youtube social">
+          <FontAwesomeIcon icon={faYoutube} size="2x" />
+        </a>
+        <a href="https://www.facebook.com/learnbuildteach/"
+          className="facebook social">
+          <FontAwesomeIcon icon={faFacebook} size="2x" />
+        </a>
+        <a href="https://www.twitter.com/jamesqquick" className="twitter social">
+          <FontAwesomeIcon icon={faTwitter} size="2x" />
+        </a>
+        <a href="https://www.instagram.com/learnbuildteach"
+          className="instagram social">
+          <FontAwesomeIcon icon={faInstagram} size="2x" />
+        </a>
       </div>
-      <div className="mt-6 border-t border-gray-100">
-        <dl className="divide-y divide-gray-100">
-          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-            <dt className="text-sm font-medium leading-6 text-gray-900">Full name</dt>
-            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">Margot Foster</dd>
-          </div>
-          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-            <dt className="text-sm font-medium leading-6 text-gray-900">Phone number</dt>
-            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">Backend Developer</dd>
-          </div>
-          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-            <dt className="text-sm font-medium leading-6 text-gray-900">Email address</dt>
-            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">margotfoster@example.com</dd>
-          </div>
-          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-            <dt className="text-sm font-medium leading-6 text-gray-900">Salary expectation</dt>
-            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">$120,000</dd>
-          </div>
-          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-            <dt className="text-sm font-medium leading-6 text-gray-900">Bio</dt>
-            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-              Fugiat ipsum ipsum deserunt culpa aute sint do nostrud anim incididunt cillum culpa consequat. Excepteur
-              qui ipsum aliquip consequat sint. Sit id mollit nulla mollit nostrud in ea officia proident. Irure nostrud
-              pariatur mollit ad adipisicing reprehenderit deserunt qui eu.
-            </dd>
-          </div>
-          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-            <dt className="text-sm font-medium leading-6 text-gray-900">Resume</dt>
-            <dd className="mt-2 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
-              <ul role="list" className="divide-y divide-gray-100 rounded-md border border-gray-200">
-                <li className="flex items-center justify-between py-4 pl-4 pr-5 text-sm leading-6">
-                  <div className="flex w-0 flex-1 items-center">
-                    <PaperClipIcon className="h-5 w-5 flex-shrink-0 text-gray-400" aria-hidden="true" />
-                    <div className="ml-4 flex min-w-0 flex-1 gap-2">
-                      <span className="truncate font-medium">resume_back_end_developer.pdf</span>
-                      <span className="flex-shrink-0 text-gray-400">2.4mb</span>
-                    </div>
-                  </div>
-                  <div className="ml-4 flex-shrink-0">
-                    <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
-                      Download
-                    </a>
-                  </div>
-                </li>
-                <li className="flex items-center justify-between py-4 pl-4 pr-5 text-sm leading-6">
-                  <div className="flex w-0 flex-1 items-center">
-                    <PaperClipIcon className="h-5 w-5 flex-shrink-0 text-gray-400" aria-hidden="true" />
-                    <div className="ml-4 flex min-w-0 flex-1 gap-2">
-                      <span className="truncate font-medium">coverletter_back_end_developer.pdf</span>
-                      <span className="flex-shrink-0 text-gray-400">4.5mb</span>
-                    </div>
-                  </div>
-                  <div className="ml-4 flex-shrink-0">
-                    <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
-                      Download
-                    </a>
-                  </div>
-                </li>
-              </ul>
-            </dd>
-          </div>
-        </dl>
-      </div>
-    </div>
-    </div>
-    // {/* // <div className="">
-    //   <div class="flex items-center justify-center w-full">
-    //     <label */}
-    //       for="dropzone-file"
-    //       class="flex flex-col items-center justify-center w-90% h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
-    //     >
-    //       <div class="flex flex-col items-center justify-center pt-5 pb-6">
-    //         <svg
-    //           class="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400"
-    //           aria-hidden="true"
-    //           xmlns="http://www.w3.org/2000/svg"
-    //           fill="none"
-    //           viewBox="0 0 20 16"
-    //         >
-    //           <path
-    //             stroke="currentColor"
-    //             stroke-linecap="round"
-    //             stroke-linejoin="round"
-    //             stroke-width="2"
-    //             d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
-    //           />
-    //         </svg>
-    //         <p class="mb-2 text-sm text-gray-500 dark:text-gray-400">
-    //           <span class="font-semibold">Click to upload Resume</span> or drag and
-    //           drop
-    //         </p>
-    //         <p class="text-xs text-gray-500 dark:text-gray-400">
-    //           PDF, DOCX, PNG, JPG
-    //         </p>
-    //       </div>
-    //       <input id="dropzone-file" type="file" class="hidden" />
-    //     </label>
-    //   </div>
-
-    //   <h1 className="text-center" style={{ marginTop: "5rem" }}>
-    //     User Profiles
-    //   </h1>
-    //   <p className="text-center mt-5">Coming Soon!!</p>
-    // </div>
-    
+      <Tabs value="bio" orientation="vertical">
+        <TabsHeader className="w-40">
+          <Tab value="bio" className="place-items-start">
+            <div className="flex items-center gap-2">
+              {React.createElement(Square3Stack3DIcon, { className: "w-5 h-5" })}
+              Bio
+            </div>
+          </Tab>
+          <Tab value="jobListing" className="place-items-start">
+            <div className="flex items-center gap-2">
+              {React.createElement(UserCircleIcon, { className: "w-5 h-5" })}
+              Job Listing
+            </div>
+          </Tab>
+          <Tab value="savedJobs" className="place-items-start">
+            <div className="flex items-center gap-2">
+              {React.createElement(Cog6ToothIcon, { className: "w-5 h-5" })}
+              Saved Jobs
+            </div>
+          </Tab>
+        </TabsHeader>
+        <TabsBody>
+          <TabPanel value="bio" className="py-0">
+            <Bio />
+          </TabPanel>
+          <TabPanel value="jobListing" className="py-0">
+            Job Listing
+          </TabPanel>
+          <TabPanel value="savedJobs" className="py-0">
+            Saved Jobs
+          </TabPanel>
+        </TabsBody>
+      </Tabs>
+    </>
   );
 }
