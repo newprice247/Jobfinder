e192a1297dfc637553bb41cc5c3d806c537656c6
yesenia <yeseniab743@gmail.com>
2023-12-10 13:23:05 -0500

    chamged size

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -40,7 +40,7 @@ export default function Header() {
           >
            <img 
            style={{
-            width:"10rem"
+            width:"5rem"
           }}
            src={logo} />
           </Link>
