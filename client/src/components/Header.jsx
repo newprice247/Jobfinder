9455360c74c92d90e78edd00b93447b57f174820
yesenia <yeseniab743@gmail.com>
2023-12-11 15:39:01 -0500

    fixing buttons

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -33,7 +33,7 @@ export default function Header() {
       <Navbar className="sticky top-0 z-10 h-max max-w-full rounded-none px-4 py-2 lg:px-8 lg:py-4 bg-myColor-1">
 
         {/* The following code is for the logo and the nav bar links */}
-        <div className="flex items-center justify-center text-blue-gray-900">
+        <div className="flex items-center justify-between text-blue-gray-900">
           <Link
             to={'/'}
             className="text-lg"
@@ -65,7 +65,7 @@ export default function Header() {
           )
           }
              {/* The following code is for the nav bar links, which are dynamically rendered based on whether the user is logged in or not */}
-            <div className="flex-items-center gap-4 justify-center">
+            <div className="flex-items-center gap-4 justify-between">
             <div className="mr-4 hidden lg:block">
               {Auth.loggedIn() ? (
                 <>
