ab58817314ba6c4f0b74f0ab26d5dc07c866eee0
Josephonate <131502920+Josephonate@users.noreply.github.com>
2023-12-11 19:25:44 -0500

    moved user resume to button and shrank upload box

--- client/src/components/ResumeUploader.jsx
+++ client/src/components/ResumeUploader.jsx
@@ -61,7 +61,7 @@ const ResumeUploader = () => {
 
   return (
     <div className="">
-      <div class="flex items-center justify-center w-full">
+      <div class="flex items-center justify-center mx-auto w-1/2">
         <label
           for="dropzone-file"
           class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
@@ -82,7 +82,7 @@ const ResumeUploader = () => {
                 d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
               />
             </svg>
-            <p class="mb-2 text-sm  m-auto text-gray-500 dark:text-gray-400">
+            <p class=" text-sm  text-gray-500 dark:text-gray-400">
               <span class="font-semibold">Click to upload Resume</span> or drag
               and drop
             </p>
@@ -100,13 +100,14 @@ const ResumeUploader = () => {
       </div>
       {/* Display the list of uploaded files */}
       <div>
-        <h2>{user.name}'s Resume</h2>
         <a 
         href={resumeUrl} 
         target="_blank" 
         rel="noreferrer"
-        className="text-myColor-2 p-2 mt-3 rounded-md bg-myColor-1 text-lg"
-        >Resume</a>
+        className="text-myColor-2 p-5 m-5 rounded-md bg-myColor-1 text-lg font-bold"
+        >
+          {user.name}'s Resume
+        </a>
       </div>
     </div>
   );
